beec6d821bb2e342b79448ee924b32761bf325bc
Rohan Batra <rohanbatra24@gmail.com>
2020-06-22 13:48:10 -0400

    remove hardcoded db var

--- server.js
+++ server.js
@@ -12,13 +12,6 @@ app.use(cors());
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-const database = {
-	users : [
-		{ id: '123', name: 'John', email: 'john@gmail.com', password: 'cookies', entries: 0, joined: new Date() },
-		{ id: '124', name: 'Sally', email: 'sally@gmail.com', password: 'bananas', entries: 0, joined: new Date() }
-	]
-};
-
 app.get('/', (req, res) => {
 	res.send(database.users);
 });
